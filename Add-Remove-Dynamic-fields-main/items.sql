ef4c767052ba7a2e9f6df0b53fcd9288af553bfe
farhanawdpf <farhanawdpf@gmail.com>
2025-01-09 11:01:32 +0600

    updatw

---
+++ Add-Remove-Dynamic-fields-main/items.sql
@@ -0,0 +1,15 @@
+use itmss;
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
